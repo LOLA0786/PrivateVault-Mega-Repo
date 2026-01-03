c5ec1ce4daf2ab9f9a862dc1bd7fff63115aa95f
LOLA0786 <info@lolasolution.com>
2026-01-03 19:13:53 +0530

    harden: extract Galani v3 into production-grade SDK modules

---
+++ src/galani/crypto/zkp.py
@@ -0,0 +1,6 @@
+class ZKProver:
+    """
+    Interface only. Production implementation may use zk-SNARKs / Bulletproofs.
+    """
+    def prove_compliance(self, value, limits):
+        raise NotImplementedError("ZKP backend not configured")
