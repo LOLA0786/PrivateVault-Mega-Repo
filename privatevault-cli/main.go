8f95c2274b73d657e4035c7aaaa7fc0f69a6e69e
Chandan Galani <info@lolasolution.com>
2026-01-23 11:07:06 +0000

    fix: clean Cobra main.go with error handling

--- privatevault-cli/main.go
+++ privatevault-cli/main.go
@@ -1,30 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/LOLA0786/PrivateVault-Mega-Repo/privatevault-cli/cmd"
+        "os"
+        "github.com/LOLA0786/PrivateVault-Mega-Repo/privatevault-cli/cmd"
 )
 
 func main() {
-	// minimal CLI:
-	// privatevault run demo
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: privatevault run demo | privatevault version")
-		os.Exit(1)
-	}
-
-	switch os.Args[1] {
-	case "run":
-		if if if err := cmd.Execute(); err != nil { os.Exit(1) }; err != nil { os.Exit(1) }(); err != nil {
-			fmt.Println("Error:", err)
-			os.Exit(1)
-		}
-	case "version":
-		fmt.Println("PrivateVault CLI dev")
-	default:
-		fmt.Println("Unknown command:", os.Args[1])
-		os.Exit(1)
-	}
+        if err := cmd.Execute(); err != nil {
+                os.Exit(1)
+        }
 }
